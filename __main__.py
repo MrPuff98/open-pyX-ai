dd5f3fa4b60128ba2981fd46807c5f43595b93d3
MrPuff98 <156108530+MrPuff98@users.noreply.github.com>
2024-06-21 19:29:44 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,17 @@
 # __main__.py
-import some_module
-import some_other_module
  
-def main():
-    # Your code here
- 
-if __name__ == "__main__":
-    main()
+'''
+    Welcome to open-pyX-ai!
+    Please, refer to /src/ for details
+    ##########################################
+    'Classic' algorithm implementation:
+    pyLearn.py - evaluates the instrumental broadening function
+    pyRLCustom.py - applies the modified Richardson-Lucy deconvolution algorithm to restore the blurred XRD pattern
+
+    ##########################################
+    Deep learning-based approach implementation:
+    genXRD_mode##.py - generates synthetic XRD patterns for further use to train DecCNN
+    pyNNLearn.py - trains and validates DecCNN on the basis of specified synthetic data sets
+    pyNNDeconv.py - deconvolves the blurred XRD pattern using pre-trained DecCNN
+    
+'''
